93fbfc19784fb067a48374e7c6400ef90f8642fd
Coletti <matheus.coletti08@gmail.com>
2023-05-11 10:34:06 -0300

    commit 11.05 (2)

---
+++ JS/AULA07/array de objetos/script.js
@@ -0,0 +1,50 @@
+let pessoa = {
+    firstName: "Matheus",
+    lastName: "Coletti",
+    idade: 15,
+    altura: "meio pá"
+   } 
+console.log(pessoa)
+
+let pessoa2 = {
+    firstName: "Ariana",
+    lastName: "Little",
+    idade: "29 quase 30, quase fechada com o matue",
+    altura: "1,56"
+   } 
+   console.log(pessoa2)
+
+let pessoa3 = {
+    firstName: "taylor",
+    lastName: "simplesmente a deusa",
+    idade: 33,
+    altura: "1,80 😮",
+   } 
+   console.log(pessoa3)
+
+   console.log()
+
+   console.log(pessoa.firstName);
+   console.log(pessoa.lastName);
+   console.log(pessoa.idade);
+   console.log(pessoa.altura);
+
+   console.log(`o ${pessoa.firstName} ${pessoa.lastName} tem ${pessoa.idade} de idade e mede ${pessoa.altura} de altura`)
+
+   console.log()
+
+   console.log(pessoa2.firstName);
+   console.log(pessoa2.lastName);
+   console.log(pessoa2.idade);
+   console.log(pessoa2.altura);
+
+   console.log(`a ${pessoa2.firstName} ${pessoa2.lastName} tem ${pessoa2.idade} de idade e mede ${pessoa2.altura} de altura`)
+
+   console.log()
+
+   console.log(pessoa3.firstName);
+   console.log(pessoa3.lastName);
+   console.log(pessoa3.idade);
+   console.log(pessoa3.altura);
+
+   console.log(`a ${pessoa3.firstName} ${pessoa3.lastName} tem ${pessoa3.idade} de idade e mede ${pessoa3.altura} de altura`)
---
+++ JS/AULA07/script.js
@@ -0,0 +1,15 @@
+let nomes = ["ermeson", "eu", "jaykay", "sean kingston"];
+console.log([])
+
+nomes.pop();
+console.log(nome);
+nome.push("Miley Cyrus");
+console.log(nomes);
+nome.push("Ivete Sangalo");
+console.log(nomes);
+nomes.shift();
+console.log(nomes);
+nomes.unshift("Tyler, The Creator");
+console.log(nomes);
+delete nomes[1]
+console.log(nomes)
